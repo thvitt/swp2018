7a58ffe678729e0a878748c6470e1647be43e246
maxnth <maximilian.noeth@protonmail.com>
2019-01-21 02:02:43 +0100

    feat: change coordinate data structur numpy array

--- src/swp2018/koordinaten_parser.py
+++ src/swp2018/koordinaten_parser.py
@@ -1,4 +1,5 @@
 from xml.dom import minidom
+import numpy as np
 
 from pathlib import Path
 
@@ -23,16 +24,16 @@ def get_coordinate_file_dict(path):
         Returns:
             A dictionary a nested dictionary where the keys of the primary dictionary represent the file names
             and the keys of the nested dictionaries represent the region identifier. The values of the  nested
-            dictionaries hold the coordinates of the corresponing region.
+            dictionaries hold the coordinates of the corresponding region which are stored in a numpy array.
         
         Example:
-           {'0001': {'r0': [('1', '1'), ('1221', '1'), ('1221', '1748'), ('1', '1748')], 'r0_012': [('338', '930'),
-           ('751', '930'), ('751', '960'), ('338', '960')]}
+           {'0001': {'r0': [[1, 1], [1221, 1], [1221, 1748], [1, 1748]], 'r0_012': [[338, 930],
+           [751, 930], [751, 960], [338, 960]]}
 
            Where:
            0001 = file name
            r0 & r0_012 = region identifier
-           [('1', '1'), ('1221', '1'), ('1221', '1748'), ('1', '1748')] = coordinates of the region
+           [[1, 1], [1221, 1], [1221, 1748], [1, 1748]] = coordinates of the region r0
     """
     
     coordinate_dict = {}
@@ -47,10 +48,8 @@ def get_coordinate_file_dict(path):
         for element in coordinate_elements:
             parent_node_name = element.parentNode.getAttribute("id")
             points_value = element.attributes["points"].value
-            coordinates = list([tuple(coo.split(",")) for coo in points_value.split()])
+            coordinates = np.array([tuple(coo.split(",")) for coo in points_value.split()]).astype(np.int_)
             temp_dict[parent_node_name] = coordinates
-
         coordinate_dict[file_name] = temp_dict
 
     return coordinate_dict
-
