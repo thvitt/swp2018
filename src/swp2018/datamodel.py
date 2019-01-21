334848eb091bc3132e5912a0a654fd3f90e5eb9e
maxnth <maximilian.noeth@protonmail.com>
2019-01-21 02:25:07 +0100

    feat: add bounding boxes to data model

--- src/swp2018/datamodel.py
+++ src/swp2018/datamodel.py
@@ -19,24 +19,25 @@ class DataModel:
     # Create Dataframes
     def create_pages_df():
         return pd.DataFrame(columns=["filename", "height", "width", "text_reg_id",
-                                     "text", "coordinates", "creator", "created", "last_change"])
+                                     "text", "coordinates", "bounding_box", "creator", "created", "last_change"])
 
     @staticmethod
     def create_textlines_df():
-        return pd.DataFrame(columns=["id", "textline", "coord_A", "coord_B", "coord_C", "coord_D", "pageNum"])
+        return pd.DataFrame(columns=["id", "textline", "coordinates", "bounding_box", "pageNum"])
 
     # Insert Data
-    def insert_page_data(self, filename, height, width, text, coordinates, text_reg_id ="no_info",
-                         creator="no_info", created = "no_info", last_change ="no_info"):
+    def insert_page_data(self, filename, height, width, text, coordinates, bounding_box, text_reg_id="no_info",
+                         creator="no_info", created="no_info", last_change="no_info"):
         
         self.pages_df = self.pages_df.append({"filename": filename, "height": height, "width": width,
                                               "text_reg_id": text_reg_id, "text": text, "coordinates": coordinates,
-                                              "creator": creator, "created": created, "lastChange": last_change},
-                                             ignore_index=True)
+                                              "bounding_box": bounding_box, "creator": creator, "created": created,
+                                              "lastChange": last_change}, ignore_index=True)
         
-    def insert_textlines_data(self, id_, textline, coordinates, pageNum):
+    def insert_textlines_data(self, id_, textline, coordinates, bounding_box, pageNum):
         
         self.textlines_df = self.textlines_df.append({"id": id_, "textline": textline, "coordinates": coordinates,
-                                                      "pageNum": pageNum}, ignore_index=True)
+                                                      "bounding_box": bounding_box, "pageNum": pageNum},
+                                                     ignore_index=True)
     
     # Get Data
