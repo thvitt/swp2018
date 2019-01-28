48d34174f7de5e6c5941bc9f91c8bfdb0748b875
Jan <janpaulus95@web.de>
2019-01-28 11:21:37 +0100

    fix: more tests with comments; tests_require to setup.py and setup.cfg

---
+++ tests/test_parser.py
@@ -0,0 +1,93 @@
+from swp2018 import parser
+import pytest
+from numpy.testing import assert_equal
+from ast import literal_eval
+
+#########################################
+#preparation
+############
+
+t_path = "resources/testcorpora/testcorpus"
+t_path2 = "resources/testcorpora/testcorpus2"
+
+def open_target_dict(path):
+    target_dict = {}
+    with open(path, mode="r") as tf:
+        target_dict = literal_eval(tf.read())
+        return target_dict
+
+def get_target_dict(path):
+    return parser.get_coordinate_file_dict(path)
+
+@pytest.fixture(params=[t_path, t_path2])
+def get_path(request):
+    return request.param
+
+#####################################
+#tests with calculate_bounding_box
+######
+
+#tests if an empty calculate_bounding_box raises an ValueError
+def test_zero_coordinates():
+    with pytest.raises(ValueError):
+        parser.calculate_bounding_box([]) == ()
+
+#tests if calculate_bounding_box get the same results as "result"
+@pytest.mark.parametrize("t_coor, result", [
+    ([(1,2), (1,4), (10,2), (10,4)], (1, 2, 10, 4)),
+    ([(7,6), (1000,6), (1000,9), (7,9)], (7, 6, 1000, 9)),
+    ([(100, 10), (100, 507), (337, 501), (337, 10)], (100, 10, 337, 507))])
+def test_coor_box(t_coor, result):
+    assert parser.calculate_bounding_box(t_coor) == result
+
+#tests if input has the wrong type
+@pytest.mark.parametrize("wrong_type", [
+    ([1, 2, 3, 4]),
+    ((1, 2, 3, 4)),
+    ({1: 2, 2: 3, 3: 4, 4: 5}),
+    (1),
+    (2.3),
+    ("3")])
+def test_bounding_box_type(wrong_type):
+    with pytest.raises(TypeError):
+        assert parser.calculate_bounding_box(wrong_type) == []
+
+#####################################
+#tests with get_coordinate_file_dict
+######
+
+#tests if get_coordinate_file generates the same dictionary for testcorpus (t_path) and testcorpus2 (t_path2)
+def test_path(get_path):
+    expected = parser.get_coordinate_file_dict(get_path)
+    result = get_target_dict(get_path)
+    assert_equal(expected, result)
+
+#tests if get_coordinate_file generates the same dictionary as the saved dictionaries passed through open_target_dict
+@pytest.mark.parametrize("path, txtdict_path", [
+    (t_path, "resources/testcorpora/target_dict.txt"),
+    (t_path2, "resources/testcorpora/target_dict2.txt")
+])
+def test_target_dict(path, txtdict_path):
+    expected = get_target_dict(path)
+    result = open_target_dict(txtdict_path)
+    assert_equal(expected, result)
+
+#tests if get_coordinate_file_dict returns empty dict when an invalid path is passed as param
+def test_nonexistent_path():
+    assert parser.get_coordinate_file_dict("Hallo") == {}
+
+@pytest.mark.parametrize("wrong_type", [
+    (1),
+    (0.1),
+    (["a", "b", "c"]),
+    ([1, 2, 3]),
+    ([0.1, 0.2, 0.3])])
+def test_type(wrong_type):
+    with pytest.raises(TypeError):
+        #parameter input: int, float
+        parser.get_coordinate_file_dict(wrong_type) == {}
+        parser.get_coordinate_file_dict(wrong_type) == open_target_dict
+
+
+
+
