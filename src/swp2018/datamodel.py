7231a86843077c7dcfa6b4fde0317417b989da4e
maxnth <maximilian.noeth@protonmail.com>
2019-01-28 13:03:30 +0100

    feat: split bbox into seperate entries in dataframe

--- src/swp2018/datamodel.py
+++ src/swp2018/datamodel.py
@@ -18,27 +18,29 @@ class DataModel:
     @staticmethod
     # Create Dataframes
     def create_pages_df():
-        return pd.DataFrame(columns=["filename", "height", "width", "text_reg_id", "reading_order",
-                                     "text", "coordinates", "bounding_box", "creator", "created", "last_change"])
+        return pd.DataFrame(columns=["filename", "height", "width", "text_reg_id", "reading_order", "text",
+                                     "coordinates", "bbox_a", "bbox_b", "bbox_c", "bbox_d", "creator", "created",
+                                     "last_change"])
 
     @staticmethod
     def create_textlines_df():
-        return pd.DataFrame(columns=["id", "textline", "coordinates", "bounding_box", "pageNum"])
+        return pd.DataFrame(columns=["id", "textline", "coordinates", "bbox_a", "bbox_b", "bbox_c", "bbox_d",
+                                     "page_num"])
 
     # Insert Data
-    def insert_page_data(self, filename, height, width, text, reading_order, coordinates, bounding_box,
+    def insert_page_data(self, filename, height, width, text, reading_order, coordinates, bbox,
                          text_reg_id="no_info", creator="no_info", created="no_info", last_change="no_info"):
         
         self.pages_df = self.pages_df.append({"filename": filename, "height": height, "width": width,
                                               "text_reg_id": text_reg_id, "text": text, "reading_order": reading_order,
-                                              "coordinates": coordinates, "bounding_box": bounding_box,
-                                              "creator": creator, "created": created, "lastChange": last_change},
-                                             ignore_index=True)
+                                              "coordinates": coordinates, "bbox_a": bbox[0], "bbox_b": bbox[1],
+                                              "bbox_c": bbox[2], "bbox_d": bbox[3], "creator": creator,
+                                              "created": created, "lastChange": last_change}, ignore_index=True)
         
-    def insert_textlines_data(self, id_, textline, coordinates, bounding_box, page_num):
+    def insert_textlines_data(self, id_, textline, coordinates, bbox, page_num):
         
         self.textlines_df = self.textlines_df.append({"id": id_, "textline": textline, "coordinates": coordinates,
-                                                      "bounding_box": bounding_box, "pageNum": page_num},
-                                                     ignore_index=True)
+                                                      "bbox_a": bbox[0], "bbox_b": bbox[1], "bbox_c": bbox[2],
+                                                      "bbox_d": bbox[3], "page_num": page_num}, ignore_index=True)
     
     # Get Data
