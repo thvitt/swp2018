bd1963f3d8a075754daf20e08eebd117c2736409
maxnth <maximilian.noeth@protonmail.com>
2019-03-31 17:09:14 +0200

    feat: replace minidom with lxml

--- src/parser.py
+++ src/parser.py
@@ -4,28 +4,27 @@
     by OCR programms like OCR4all and to extract and structure coordinates. 
 
 """
+from lxml import etree
+import numpy as np
+
+from pathlib import Path
+
 import logging
 from logging_config import setup_logging
 setup_logging()
 logger = logging.getLogger(__name__)
 parser_log = logging.getLogger('parser_logger')
 
-import xml.dom.minidom as minidom
-import numpy as np
-
-from pathlib import Path
-
-
 
 def generate_file_list(path, file_extension=".xml"):
     """
-    
+
     Generates a generator with all files that will be processed.
 
     Args:
         path (str) -- Path to the desired directory.
         file_extension (str) -- File extension to search for.
-        
+
     Returns:
         Generator which contains Path objects to all XML files in the given directory and its subdirectories.
 
@@ -35,17 +34,17 @@ def generate_file_list(path, file_extension=".xml"):
 
 def parse_xml_structure(path):
     """
-        Parses XML file and creates a nested dictionary that contains the filename and the 
+        Parses XML file and creates a nested dictionary that contains the filename and the
         text region id that belongs to this file as well as the coordinates of the text region:
-    
+
         Args:
             path (str) -- Path to the desired directory.
-            
+
         Returns:
             A nested dictionary where the keys of the primary dictionary represent the file names
             and the keys of the nested dictionaries represent the region identifier. The values of the  nested
             dictionaries hold the coordinates of the corresponding region which are stored in a numpy array.
-        
+
         Example:
            {'0001': {'r0': [[1, 1], [1221, 1], [1221, 1748], [1, 1748]], 'r0_012': [[338, 930],
            [751, 930], [751, 960], [338, 960]]}
@@ -55,19 +54,21 @@ def parse_xml_structure(path):
            "r0" & "r0_012" = region identifier
            [[1, 1], [1221, 1], [1221, 1748], [1, 1748]] = coordinates of the region r0
     """
-    
+
     coordinate_dict = {}
+
+    pagexml_ns = {"gts": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2017-07-15"}
 
     try:
         for file in generate_file_list(path):
             file_name = file.stem
-            xml_text = minidom.parse(str(file))
-            coordinate_elements = xml_text.getElementsByTagName("Coords")
+            xml_text = etree.parse(file.as_posix())
+            coordinate_elements = xml_text.findall(".//gts:Coords", namespaces=pagexml_ns)
 
             temp_dict = {}
 
             for element in coordinate_elements:
-                parent_node_name = element.parentNode.getAttribute("id")
+                parent_node_name = element.getparent().get("id")
                 temp_dict[parent_node_name] = extract_coordinates(element)
             coordinate_dict[file_name] = temp_dict
             parser_log.info('Successfully created coordinate_dict')
@@ -77,18 +78,18 @@ def parse_xml_structure(path):
 
     return coordinate_dict
 
-#%%
+
 def extract_coordinates(xml_element):
     """Extracts the coordinates from the @point-attribute of a given XML-element and returns them as a numpay array.
 
     Args:
-        xml_element (string) -- Name of the XML-element.       
+        xml_element (string) -- Name of the XML-element.
     Returns:
         Numpy Array containing the bounding box coordinates.
-        
+
     """
     try:
-        points_value = xml_element.attributes["points"].value
+        points_value = xml_element.get("points")
         coordinates = np.array([tuple(coo.split(",")) for coo in points_value.split()]).astype(np.int_)
         parser_log.info('Successfully extracted coordinates.')
     except Exception as e:
@@ -103,7 +104,7 @@ def calculate_bounding_box(coordinates):
     Args:
         coordinates (list) -- A list of coordinates which are stored in tuples (x, y).
     Returns:
-        Tuple containing the bounding box coordinates.     
+        Tuple containing the bounding box coordinates.
     """
 
     if len(coordinates) == 0:
